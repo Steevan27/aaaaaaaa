2fc725adaaeaa1281caa4bcae8cf1b3cd819fa1d
Steevan27 <steevanmahen@gmail.com>
2022-11-30 20:59:02 +0530

    master

---
+++ src/org/sss/testing/Classa.java
@@ -0,0 +1,31 @@
+package org.sss.testing;
+
+public class Classa {
+	
+	
+public static void main(String[] args) {
+	
+	int a=10;
+	
+//	int b=a%2;
+//	System.out.println(b);
+
+	if (a%2==0) {
+		
+		System.out.println("even number");
+		
+	} else {
+         System.out.println("odd num");
+	}
+	
+	
+	
+	
+	
+	
+}	
+	
+	
+	
+
+}
---
+++ src/org/sss/testing/steevanms.java
@@ -0,0 +1,42 @@
+package org.sss.testing;
+
+import java.util.Date;
+
+public class steevanms {
+
+
+	public static void main(String[] args) {
+
+		
+		
+		Date i = new Date ();
+		System.out.println(i);
+		
+		
+		//NESTED FOR LOOP
+		
+		System.out.println("Price List");
+		for (int j = 1; j <= 8; j++) {
+			for (int k = 1; k <= j; k++) {
+
+			
+				System.out.print("* ");
+				
+			}
+	
+
+			System.out.println();
+
+		}
+		
+		
+
+		System.out.println("END");
+	}
+
+	
+	
+	
+	
+	
+}
